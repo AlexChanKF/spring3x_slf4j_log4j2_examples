e765f9c7808093bd278eb2995a9495132b9d9e0c
Alex KF Chan <alexkfchan@dahsing.com>
2022-01-06 16:12:44 +0800

    Init Version

--- src/main/java/com/mkyong/core/HelloWorld.java
+++ src/main/java/com/mkyong/core/HelloWorld.java
@@ -1,15 +1,11 @@
 package com.mkyong.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Spring bean
  */
 
 public class HelloWorld {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorld.class);
     private String name;
 
     public void setName(String name) {
@@ -18,11 +14,6 @@ public class HelloWorld {
 
     public void printHello() {
         String message = "Spring 3 : Hello ! " + name;
-        LOGGER.trace("trace level " + message);
-        LOGGER.debug("debug level " + message);
-        LOGGER.info("info level " + message);
-        LOGGER.warn("warn level " + message);
-        LOGGER.error("error level " + message);
         System.out.println("console level" + message);
     }
 
